8fbe5f0538e993c1c08b636cf760d6dce1fcaeee
bergie15 <Bergie.Bergstrom@gmail.com>
2025-09-03 18:40:43 -0700

    fixing card spacing

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
 
 export interface Recipe {
   title: string;
@@ -18,42 +18,57 @@ interface CardsProps {
 const Cards: React.FC<CardsProps> = ({ recipes }) => {
   return (
     <Grid container spacing={3} justifyContent="center">
-      {recipes.map((recipe, idx) => {
-        // If odd number and last item, don't center
-        const isLast = idx === recipes.length - 1;
-        const isOdd = recipes.length % 2 === 1;
-        return (
-          <Grid
-            item
-            key={idx}
-            xs={12}
-            sm={6}
-            md={4}
-            lg={3}
-            sx={isLast && isOdd ? { marginLeft: 'auto', marginRight: 0 } : {}}
+      {recipes.map((recipe, idx) => (
+        <Grid item key={idx} xs={12} sm={6} md={4} lg={3}>
+          <Card
+            sx={{
+              borderRadius: 3,
+              boxShadow: 3,
+              width: 260,
+              height: 340,
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "flex-start",
+              alignItems: "stretch",
+            }}
           >
-            <Card sx={{ borderRadius: 3, boxShadow: 3, height: 320, display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', minHeight: 320, maxHeight: 320 }}>
-              {recipe.image && (
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image={recipe.image}
-                  alt={recipe.title}
-                  sx={{ objectFit: 'cover' }}
-                />
-              )}
-              <CardContent sx={{ flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
-                <Typography gutterBottom variant="h6" component="div" noWrap>
-                  {recipe.title}
-                </Typography>
-                <Typography variant="body2" color="text.secondary" sx={{ overflow: 'hidden', textOverflow: 'ellipsis', display: '-webkit-box', WebkitLineClamp: 3, WebkitBoxOrient: 'vertical' }}>
-                  {recipe.description}
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-        );
-      })}
+            {recipe.image && (
+              <CardMedia
+                component="img"
+                image={recipe.image}
+                alt={recipe.title}
+                sx={{ objectFit: "cover", width: "100%", height: 140 }}
+              />
+            )}
+            <CardContent
+              sx={{
+                flex: 1,
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+                minHeight: 0,
+              }}
+            >
+              <Typography gutterBottom variant="h6" component="div" noWrap>
+                {recipe.title}
+              </Typography>
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                sx={{
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                  display: "-webkit-box",
+                  WebkitLineClamp: 3,
+                  WebkitBoxOrient: "vertical",
+                }}
+              >
+                {recipe.description}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      ))}
     </Grid>
   );
 };
